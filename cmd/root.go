dc8a2cb94c2c76090d718fb2eaaf439f67bcc4f4
davidonlinearchive <davidonlinearchive@gmail.com>
2025-10-17 15:40:29 -0400

    refactor(root.go): removed unused //init function

--- cmd/root.go
+++ cmd/root.go
@@ -14,9 +14,6 @@ var rootCmd = &cobra.Command{
 	Use:   "scurl [url]",
 	Short: "Scurl (Simple-curl) is a CLI tool used for transferring data",
 	Args:  cobra.ExactArgs(1),
-
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
 		url := args[0]
 		SendGetRequest(url)
@@ -30,16 +27,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.scurl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
