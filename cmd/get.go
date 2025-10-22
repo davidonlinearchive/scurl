3102ef036870f9ea77c91bc558b0a9004c2db64a
davidonlinearchive <davidonlinearchive@gmail.com>
2025-10-22 18:49:19 -0400

    feat(head.go) add HEAD HTTP request

--- cmd/get.go
+++ cmd/get.go
@@ -4,7 +4,6 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -17,40 +16,32 @@ import (
 func SendGetRequest(url string) {
 	c := &http.Client{Timeout: 30 * time.Second} /*  Timeout client request after 30 seconds */
 
-	res, err := c.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatalln("Failed to get domain ", err)
+		log.Fatalf("Error creating request: %v", err)
 	}
-	defer res.Body.Close()
 
-	_, err = io.Copy(os.Stdout, res.Body)
+	resp, err := c.Do(req)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		log.Fatalf("Request failed: %v", err)
 	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(os.Stdout, resp.Body)
+
 }
 
-// getCmd represents the get command
+/*  getCmd represents the GET command */
 var getCmd = &cobra.Command{
 	Use:   "GET",
 	Short: "Perform a HTTP get request",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get command called") // remove line later; shows GET command called
-		url := args[0]                    // args[0] not os.Args[0]
+		url := args[0] /* args[0] != os.Args[0] */
 		SendGetRequest(url)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ cmd/head.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+func sendHeadRequest(url string) {
+	c := &http.Client{Timeout: 30 * time.Second}
+
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		log.Fatalf("Error creating request %v", err)
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatalf("Request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	for key, values := range resp.Header {
+		for _, v := range values {
+			fmt.Printf("%s: %s\n", key, v)
+		}
+	}
+}
+
+// headCmd represents the head command
+var headCmd = &cobra.Command{
+	Use:   "HEAD",
+	Short: "Perform a HTTP head request",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("head called")
+		url := args[0]
+		sendHeadRequest(url)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(headCmd)
+}
